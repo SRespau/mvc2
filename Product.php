4dc552e07cbbb928ea0c5b8a0d0961bfbe7b17c2
Sergio Respau <sergiorespau@gmail.com>
2022-11-07 17:31:00 +0100

    primer ejercicio

---
+++ Product.php
@@ -0,0 +1,26 @@
+<?php
+    //Fichero que simula el modelo con datos
+    class Product{
+        const PRODUCTS = [
+            array(1, "Cortacesped"),
+            array(2, "Pizarra"),
+            array(3, "Billar"),
+            array(4, "Dardos")
+        ];
+
+    function __construct(){
+        //constructor vacio
+    } 
+     
+    //Devuelve todos los productos
+    public static function all(){
+        return Product::PRODUCTS; //Devolvemos de la clase product la constante PRODUCT. :: por ser estatico
+    }        
+     
+    //Devuelve un producto en particular buscado por la clave del array
+    public function find($id){
+        return Product::PRODUCTS[$id - 1];
+    }
+
+
+    }// FIN_CLASE
